55f460f97c12a97a9e6ed7b4d17eadad85499725
aklileseyoum <75662851+aklileseyoum@users.noreply.github.com>
2022-09-28 02:32:33 +0300

    Update FizzBuzz.py

--- progress_sheet/FizzBuzz.py
+++ progress_sheet/FizzBuzz.py
@@ -7,13 +7,13 @@ class Solution(object):
         result = []
         
         for i in range(1, n+1):
-            if i% 3==0 and i% 5==0:
-                result.append("FizzBuzz")
-            elif i % 3 == 0:
-                result.append("Fizz")
+            if i % 3 == 0:
+                if i % 5 == 0:
+                    result.append("FizzBuzz")
+                else:
+                    result.append("Fizz")
             elif i % 5 == 0:
                 result.append("Buzz")
             else:
                 result.append(str(i))
         return result
-        
