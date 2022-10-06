aeacab92f61753048128d75e703ec9af076113de
aklileseyoum <75662851+aklileseyoum@users.noreply.github.com>
2022-10-06 19:50:59 +0300

    Create product_of_array_except_self.py

---
+++ progress_sheet/product_of_array_except_self.py
@@ -0,0 +1,23 @@
+import numpy
+class Solution(object):
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        n = len(nums)
+        left = [0] * n
+        right = [0] * n
+        answer = [0] * n
+        left[0] = 1
+        for i in range(1, n):
+            left[i] = nums[i - 1] * left[i - 1]
+            
+        right[n - 1] = 1
+        for i in range(n-2, -1, -1):
+            right[i] = nums[i+1] * right[i+1]
+            
+        for i in range(n):
+            answer[i] = left[i] * right[i]
+            
+        return answer
